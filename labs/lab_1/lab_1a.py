a69ac6cc1a04eaf1f9518bd768b48641aedc513c
Dev-Goda <devgoda1111@gmail.com>
2026-02-12 23:29:43 -0500

    Update lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,7 +11,7 @@ def main():
     name = "Dev Goda" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-    print("Hello, my name is Dev Goda. I am a junior from Chantilly High School in Northern Virginia. I love everything model aircraft and UAVs and have been building and flying RC aircraft since I was 5 years old. Recently I have also gotten really good at free flight competitions and was the national champion of both indoor and outdoor free flight last year in my age category.")
+    print("Hello,  my name is Dev Goda. I am a junior from Chantilly High School in Northern Virginia. I love everything model aircraft and UAVs and have been building and flying RC aircraft since I was 5 years old. Recently I have also gotten really good at free flight competitions and was the national champion of both indoor and outdoor free flight last year in my age category.")
 
 if __name__ == "__main__":
     main()
